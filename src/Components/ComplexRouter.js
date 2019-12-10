33c8854b83dbaf094e05881d671d116ba9eb61e7
Michael Dennis <michael.dennis@paid.com>
2019-12-09 21:16:20 -0700

    debugging Complex Router issues.

--- src/Components/ComplexRouter.js
+++ src/Components/ComplexRouter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { withRouter, Link } from "react-router-dom";
+import { withRouter, Link, Switch, Route } from "react-router-dom";
 import {AppContext} from "../AppContext";
 export default withRouter(function ComplexRouter(props) {
   const context = React.useContext(AppContext);
@@ -16,12 +16,35 @@ export default withRouter(function ComplexRouter(props) {
           </Link>
         ) : null}
         <button onClick={props.history.goBack.bind(this)}>Go Back</button>
-        <Switch basename="/complex-router">
-          <Route to="/a" exact component={() => <p>Nest As Deep As You Want</p>} />
-          <Route to="/b" component={() => <p>Keep It Simple, and Organized.</p>} />
-          <Route to="/c" component={() => <p>Reusable Components make building simple!</p>} />
+        <Switch>
+          <Route
+            path="/complex-router/a"
+            exact
+            component={A}
+          />
+          <Route
+            path="/complex-router/b"
+            component={() => <p>Keep It Simple, and Organized.</p>}
+          />
+          <Route
+            path="/complex-router/c"
+            component={() => <p>Reusable Components make building simple!</p>}
+          />
         </Switch>
+        <Link to="/complex-router/a">
+          <button type="button">A</button>
+        </Link>
+        <Link to="/complex-router/b">
+          <button type="button">B</button>
+        </Link>
+        <Link to="/complex-router/c">
+          <button type="button">C</button>
+        </Link>
       </header>
     </div>
   );
 });
+
+function A() {
+  return <p>Nest As Deep As You Want</p>;
+}
